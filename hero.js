c043c8ab023e9d048db19a1ac4c482a18ebd90e9
kongxinga <kongxinga@gmail.com>
2014-10-14 19:05:49 -0400

    bah

--- hero.js
+++ hero.js
@@ -67,20 +67,147 @@
 /*if in group and see injured, heal them*/
 /*if enemy higher health run*/
 /*if injured run to group or fountain*/
- 
+ /*
   var move = function(gameData, helpers) {
    var myHero = gameData.activeHero;
    if (myHero.health < 60) {
      return helpers.findNearestHealthWell(gameData);
    } else if (myHero.health>=90) {
-     return helpers.findNearestWeakerEnemy(gamedata); /*prioritize uncapped enemy mines later?*/
+     return helpers.findNearestWeakerEnemy(gamedata); 
    } else {
 	return helpers.findNearestTeamMember(gamedata);
    }
    
  };
+ */
  
  
+ 
+ /*the careful drunkard*/
+  var move = function(gameData, helpers) {
+   var myHero = gameData.activeHero;
+   
+   var medicCall=60;
+   var painThreshold=70;
+   var bloodLust=30;
+   var blooderLust=20;
+   
+   
+   //Get nearest health well stats
+  var healthWellStats = helpers.findNearestObjectDirectionAndDistance(gameData.board, myHero, function(boardTile) {
+    if (boardTile.type === 'HealthWell') {
+       return true;
+    }
+  });
+  
+  //Get nearest grave stats
+var graveStats = helpers.findNearestObjectDirectionAndDistance(gameData.board, myHero, function(boardTile) {
+    if (boardTile.subType === 'Bones') {
+       return true;
+    }
+  });
+
+  
+  
+  // Get friendly dying hero stats
+  var teamHeroStats = helpers.findNearestObjectDirectionAndDistance(gameData.board, myHero, function(heroTile) {
+    return heroTile.type === 'Hero' && heroTile.team === myHero.team && heroTile.health <= medicCall;
+  });
+  
+  
+  //return tree stats to make less drunk
+ var treeStats = helpers.findNearestObjectDirectionAndDistance(gameData.board, myHero, function(boardTile) {
+    if (boardTile.subType === 'Tree') {
+       return true;
+    }
+  });
+ 
+  
+ 
+  
+  
+  
+  //get nearest critical enemy stats
+  
+    var enemyHeroStats = helpers.findNearestObjectDirectionAndDistance(gameData.board, myHero, function(heroTile) {
+    return heroTile.type === 'Hero' && heroTile.team !== myHero.team && heroTile.health <= bloodLust;
+  });
+  
+  
+  //This is my plan to swipe kill enemy heroes that can be killed by AOE attacks
+    var enemyKaputHeroStats = helpers.findNearestObjectDirectionAndDistance(gameData.board, myHero, function(heroTile) {
+    return heroTile.type === 'Hero' && heroTile.team !== myHero.team && heroTile.health <= blooderLust;
+  });
+  
+  
+  var distanceToDyingHero = teamHeroStats.distance;
+  var directionToDyingHero = teamHeroStats.direction;
+  
+  var distanceToHealthWell = healthWellStats.distance;
+  var directionToHealthWell = healthWellStats.direction;
+  
+  var distanceToGrave = graveStats.distance;
+  var directionToGrave = graveStats.direction;
+  
+  var distanceToTree = treeStats.distance;
+  var directionToTree = treeStats.direction;
+  
+  var distanceToBadHero = enemyHeroStats.distance;
+  var directionToBadHero = enemyHeroStats.direction;
+  
+  var distanceToBadKaputHero = enemyKaputHeroStats.distance;
+  var directionToBadKaputHero = enemyKaputHeroStats.direction;
+   
+   
+   if (myHero.health <= painThreshold) {
+     return helpers.findNearestHealthWell(gameData);
+     
+   } else if (myHero.health < 100 && distanceToHealthWell === 1) {
+    //Heal if you aren't full health and are close to a health well already.
+    //TOPOFF
+    return directionToHealthWell;
+  } else if (distanceToDyingHero === 1){
+    //heal dying friends
+    return directionToDyingHero;
+    
+  } else if (distanceToBadHero === 1){
+    //direct attacks to heroes we can kill in one blow
+    return directionToBadHero;    
+ 
+  } else if (distanceToBadKaputHero === 2){
+    //Move and swipe wimpier heroes
+    return directionToBadHero;     
+    
+  } else if (distanceToGrave === 1){
+    //Rob a grave why not
+    return directionToGrave;  	
+	
+    
+    
+   } else {
+        var choices = ['North', 'South', 'East', 'West'];
+		   var firstChoice= choices[Math.floor(Math.random()*4)];
+		   
+		   if (firstChoice===directionToTree &&){
+		   //if the tree is near and we chose tree, truncate the array and choose again
+				var newArray = [];
+				for(var i=0;i<choices.length;i++)
+				if(choices[i]!==firstChoice) newArray.push(a[i]);
+		   
+				return newArray[Math.floor(Math.random()*3)];
+		   
+		   
+		   }
+		else {
+		
+		
+   return choices[Math.floor(Math.random()*4)];
+   }
+   }
+ };
+   
+//why do people uglify their code. You should be confident in the superiority of your design 
+//without relying on obfuscation
  
 // // The "Unwise Assassin"
 // // This hero will attempt to kill the closest enemy hero. No matter what.
